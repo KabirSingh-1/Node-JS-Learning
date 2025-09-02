14f8faf0d4e27822c9f2f5e1dccdd1ec2f139388
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-02 15:50:37 +0530

    changes

--- HttpServer/index.js
+++ HttpServer/index.js
@@ -1,13 +1,18 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 const myServer = http.createServer((req ,res) => {
-    const log = `${Date.now()} :${req.url} New Req Received\n`;
+    const log = `${Date.now()} : ${req.method}  ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err ,data)=>{
         // res.end("Hello From Server");
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/': res.end("HomePage");
             break
-            case '/about':res.end("I am Kabir Singh")
+            case '/about':
+                const username = myUrl.query.myname;
+                res.end(`I am ${username}`)
             break
             default: res.end("404 Not Found")
         }
