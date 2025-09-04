2b7259ce304562f907d354c62fa0d0e475a13b55
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-04 10:32:53 +0530

    index.js

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,4 +1,19 @@
 const express  = require("express");
+const users = require("./MOCK_DATA.json");
 const app = express();
-const port = 8000;
-app.listen(PORT,() => console.log(``))
+const PORT = 8000;
+
+// route 
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+
+app.get("/users",(req ,res)=>{
+    const html = `
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
+app.listen(PORT,() => console.log(`Server Started at PORT : ${PORT}`))
