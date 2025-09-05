3f174c05fc0cd56a35e4e516f6ec0beff756aa88
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-05 14:41:19 +0530

    changes

--- Short-Url/index.js
+++ Short-Url/index.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express();
 
+const urlRoute = require("./routes/url")
+
+app.use("url",urlRoute);
 const PORT = 8001
 app.listen(PORT,()=>console.log(`Server Started at PORT: ${PORT}`))
