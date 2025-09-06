fcdf6531d6dee44887c41072cdf2dcbe5be0a18e
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-06 13:34:42 +0530

    changes

--- Short-Url/index.js
+++ Short-Url/index.js
@@ -1,14 +1,31 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const {connectToMongoDB} = require("./connect");
+const path = require('path');
+const { connectToMongoDB } = require("./connect");
 
-const urlRoute = require("./routes/url")
+const urlRoute = require("./routes/url");
+const URL = require("./models/Url");
 
-connectToMongoDB('mongodb://localhost:27017/short-url')
-.then(()=> console.log("mongoDB Has Connected"))
 
+connectToMongoDB("mongodb://localhost:27017/short-url")
+  .then(() => console.log("✅ MongoDB Has Connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+app.set("view engine","ejs");
+app.set('views', path.resolve("./views") );
 app.use(express.json());
 
-app.use("/url",urlRoute);
-const PORT = 8001
-app.listen(PORT,()=>console.log(`Server Started at PORT: ${PORT}`))
+// test route
+app.get("/test", async(req, res) => {
+  const allUrls = await URL.find({});
+//   res.send("<h1>Hey from server</h1>");
+return res.render('Home',{
+  urls: allUrls,
+})
+});
+
+// url shortener routes
+app.use("/url", urlRoute);
+
+const PORT = 8001;
+app.listen(PORT, () => console.log(`🚀 Server Started at PORT: ${PORT}`));
