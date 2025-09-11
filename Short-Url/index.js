4f55d7467ed53468d557eeac9e725e642960f671
KabirSingh-1 <singhkabir44778@gmail.com>
2025-09-11 15:08:58 +0530

    changes

--- Short-Url/index.js
+++ Short-Url/index.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const path = require('path');
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require('./routes/url')
-const staticRoute = require('./routes/staticRouter')
-const URL = require('./models/url')
+const URL = require('./models/url');
+
+
+const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
 const app = express();
 const PORT = 8001;
 // Connect to MongoDB
 connectToMongoDB("mongodb://localhost:27017/short-url")
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 app.set("view engine","ejs");
 app.set('views', path.resolve("./views"));
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/",staticRoute);
+app.use("/",staticRoute);
+app.use("/url", urlRoute);
+app.use("/user", userRoute);
+
 app.get('/test', async (req, res) => {
   const allUrls = await URL.find({});
   return res.render("Home",{
     urls : allUrls,
   });
 });
-app.use("/url", urlRoute);
 app.get('/url/:shortId', async (req, res) => {
   const shortId = req.params.shortId;
 
